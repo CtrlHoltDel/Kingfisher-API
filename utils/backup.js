a273cefa1b7213cfc1fd8efd0410ed66388f6ded
CtrlHoltDel <ctrlholtdel@gmail.com>
2022-01-19 10:05:25 +0000

    Added missing try/catch

--- utils/backup.js
+++ utils/backup.js
@@ -2,52 +2,62 @@ const fs = require("fs/promises");
 const { allItems } = require("./db");
 
 exports.generateBackups = async () => {
-  const players = await allItems("players");
-  const notes = await allItems("notes");
-  const tendencies = await allItems("tendencies");
+  try {
+    const players = await allItems("players");
+    const notes = await allItems("notes");
+    const tendencies = await allItems("tendencies");
 
-  const playersCSVstring = players.reduce(
-    (total, { player_name, type, created_at }) =>
-      (total += `${player_name}, ${type},${created_at}\n`),
-    `player_name, type, created_at\n`
-  );
-
-  const notesCSVstring = notes.reduce(
-    (total, { note_id, player_name, created_by, created_at, note }) =>
-      (total += `${note_id}, ${player_name}, ${created_by}, ${created_at}, ${note.replaceAll(
-        ",",
-        "  |||"
-      )}\n`),
-    `note_id, player_name, created_by, created_at, note\n`
-  );
-
-  const tendenciesCSVstring = tendencies.reduce(
-    (total, { tendency_id, player_name, created_by, created_at, tendency }) =>
-      (total += `${tendency_id}, ${player_name}, ${created_by}, ${created_at}, ${tendency.replaceAll(
-        ",",
-        "  |||"
-      )}\n`),
-    `tendency_id, player_name, created_by, created_at, tendency\n`
-  );
-
-  let allData = players.map((player) => {
-    const filteredNotes = notes.filter(
-      (note) => note.player_name === player.player_name
+    const playersCSVstring = players.reduce(
+      (total, { player_name, type, created_at }) =>
+        (total += `${player_name}, ${type},${created_at}\n`),
+      `player_name, type, created_at\n`
     );
 
-    const filteredTendencies = tendencies.filter(
-      (tendency) => tendency.player_name === player.player_name
+    const notesCSVstring = notes.reduce(
+      (total, { note_id, player_name, created_by, created_at, note }) =>
+        (total += `${note_id}, ${player_name}, ${created_by}, ${created_at}, ${note.replaceAll(
+          ",",
+          "  |||"
+        )}\n`),
+      `note_id, player_name, created_by, created_at, note\n`
     );
 
-    return { ...player, notes: filteredNotes, tendencies: filteredTendencies };
-  });
+    const tendenciesCSVstring = tendencies.reduce(
+      (total, { tendency_id, player_name, created_by, created_at, tendency }) =>
+        (total += `${tendency_id}, ${player_name}, ${created_by}, ${created_at}, ${tendency.replaceAll(
+          ",",
+          "  |||"
+        )}\n`),
+      `tendency_id, player_name, created_by, created_at, tendency\n`
+    );
 
-  const directory = `${__dirname.slice(0, -6)}`;
+    let allData = players.map((player) => {
+      const filteredNotes = notes.filter(
+        (note) => note.player_name === player.player_name
+      );
 
-  await fs.rm(directory + "/backup", { recursive: true, force: true });
-  await fs.mkdir(directory + "/backup");
-  await fs.writeFile(directory + "/backup/players.csv", playersCSVstring);
-  await fs.writeFile(directory + "/backup/notes.csv", notesCSVstring);
-  await fs.writeFile(directory + "/backup/tendencies.csv", tendenciesCSVstring);
-  await fs.writeFile(directory + "/backup/all.json", JSON.stringify(allData));
+      const filteredTendencies = tendencies.filter(
+        (tendency) => tendency.player_name === player.player_name
+      );
+
+      return {
+        ...player,
+        notes: filteredNotes,
+        tendencies: filteredTendencies,
+      };
+    });
+
+    const directory = `${__dirname.slice(0, -6)}`;
+    await fs.rm(directory + "/backup", { recursive: true, force: true });
+    await fs.mkdir(directory + "/backup");
+    await fs.writeFile(directory + "/backup/players.csv", playersCSVstring);
+    await fs.writeFile(directory + "/backup/notes.csv", notesCSVstring);
+    await fs.writeFile(
+      directory + "/backup/tendencies.csv",
+      tendenciesCSVstring
+    );
+    await fs.writeFile(directory + "/backup/all.json", JSON.stringify(allData));
+  } catch (err) {
+    console.log(err);
+  }
 };
